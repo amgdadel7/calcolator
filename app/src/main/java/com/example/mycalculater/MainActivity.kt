b897c7afc62b36fc9af5ec39260f5bce872868df
amjad alhakimi <amgdadel7@gmail.com>
2022-04-13 14:57:10 +0300

    final

--- app/src/main/java/com/example/mycalculater/MainActivity.kt
+++ app/src/main/java/com/example/mycalculater/MainActivity.kt
@@ -2,12 +2,12 @@ package com.example.mycalculater
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.TouchDelegate
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.example.mycalculater.databinding.ActivityMainBinding
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
     var LastNumaric : Boolean = false
@@ -43,7 +43,92 @@ class MainActivity : AppCompatActivity() {
             LastNumaric =false
             LastDot = true
 
-        }
 
+
+        }
+    }
+    fun onOperattor(view : View){
+        binding.tvInput?.text?.let {
+
+            if (LastNumaric && !isOperatorAdded(it.toString())){
+                binding.tvInput.append((view as Button).text)
+                LastNumaric = false
+                LastDot =false
+            }
+        }
+    }
+    fun onEqual(view: View){
+        if (LastNumaric){
+            var tvVal = binding.tvInput.text.toString()
+            var prefix =""
+            try {
+                if(tvVal.startsWith("-")){
+                    prefix =""
+
+                    tvVal =tvVal.substring(1)
+                }
+                if(tvVal.contains("-")){
+                    val splitVal = tvVal.split("-")
+                    var one = splitVal[0]
+                    var two = splitVal[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    binding.tvInput.text =removeZeroAfterDot((one.toDouble()-two.toDouble()).toString())
+                }else if(tvVal.contains("+")){
+                    val splitVal = tvVal.split("+")
+                    var one = splitVal[0]
+                    var two = splitVal[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    binding.tvInput.text =removeZeroAfterDot((one.toDouble()+two.toDouble()).toString())
+                }else if(tvVal.contains("*")){
+                    val splitVal = tvVal.split("*")
+                    var one = splitVal[0]
+                    var two = splitVal[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    binding.tvInput.text =removeZeroAfterDot((one.toDouble()*two.toDouble()).toString())
+                }else if(tvVal.contains("/")){
+                    val splitVal = tvVal.split("/")
+                    var one = splitVal[0]
+                    var two = splitVal[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    binding.tvInput.text =removeZeroAfterDot((one.toDouble()/two.toDouble()).toString())
+                }
+
+            }
+            catch (e: ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+    }
+    private fun removeZeroAfterDot(result :String) : String{
+        var value = result
+        if(result.contains(".0"))
+            value = result.substring(0,result.length - 2)
+        return value
+    }
+    private fun isOperatorAdded(value :String) : Boolean{
+        return if(value.startsWith("-")){
+            false
+        }else{
+            value.contains("/")
+                    || value.contains("*")
+                    ||value.contains("+")
+                    ||value.contains("-")
+        }
     }
 }
